14d0c3c626ba205247b45b464b27265e7e3ef739
santiagopemo <santiagopemo@gmail.com>
2020-04-30 17:24:48 +0000

    corrected output order

--- 0x02-python-import_modules/1-calculation.py
+++ 0x02-python-import_modules/1-calculation.py
@@ -3,6 +3,7 @@ if __name__ == "__main__":
     from calculator_1 import add, sub, mul, div
     a = 10
     b = 5
-    operation = {"+": add, "-": sub, "*": mul, "/": div}
-    for op in operation:
-        print("{:d} {:s} {:d} = {:d}".format(a, op, b, operation[op](a, b)))
+    op = ("+", "-", "*", "/")
+    fu = (add, sub, mul, div)
+    for i in range(4):
+        print("{:d} {:s} {:d} = {:d}".format(a, op[i], b, fu[i](a, b)))
