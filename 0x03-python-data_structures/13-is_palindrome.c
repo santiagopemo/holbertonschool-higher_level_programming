5df96e9d99f06eb609061022944bac0501d5dac6
santiagopemo <santiagopemo@gmail.com>
2020-05-06 01:58:01 +0000

    added task 13

--- 0x03-python-data_structures/13-is_palindrome.c
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -2,6 +2,27 @@
 #include <stdlib.h>
 
 /**
+ * reverse_listint - function that reverses a listint_t linked list
+ * @head: double pointer to a list
+ *
+ * Return: pointer to the first node of the reversed list
+ */
+listint_t *reverse_listint(listint_t **head)
+{
+	listint_t *prev, *next;
+
+	prev = NULL;
+	for (; *head; *head = next)
+	{
+		next = (*head)->next;
+		(*head)->next = prev;
+		prev = *head;
+	}
+	*head = prev;
+	return (*head);
+}
+
+/**
  * is_palindrome - checks if a singly linked list is a palindrome
  * @head: double pointer to a listint_t linked list head
  *
@@ -9,19 +30,23 @@
  */
 int is_palindrome(listint_t **head)
 {
-	listint_t *h = *head, *t;
-	int size, i, j;
+	listint_t *h = *head, *r, *rhead;
+	int size, i;
 
 	if (*head == NULL)
 		return (1);
 	for (size = 0; h->next; h = h->next, size++)
 		continue;
-	for (t = *head, i = 0; i <= size; i++, t = t->next)
+	for (h = *head, i = 0; i <= (size / 2); i++, h = h->next)
+		continue;
+	h = h->next;
+	rhead = reverse_listint(&h);
+	r = rhead;
+	for (h = *head; r; h = h->next, r = r->next)
 	{
-		for (h = *head, j = 0; j < size - i; j++, h = h->next)
-			continue;
-		if (t->n != h->n)
+		if (h->n != r->n)
 			return (0);
 	}
+	reverse_listint(&rhead);
 	return (1);
 }
