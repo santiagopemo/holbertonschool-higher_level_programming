4439964ade46aceeaac40ede02782a06dd7cdb81
santiagopemo <santiagopemo@gmail.com>
2020-05-07 23:00:02 +0000

    corrected ()

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
 def best_score(a_dictionary):
     if a_dictionary and len(a_dictionary):
-        return sorted(a_dictionary.items())[-1][0]
+        return max(a_dictionary, key = a_dictionary.get)
