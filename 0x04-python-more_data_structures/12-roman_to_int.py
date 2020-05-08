4dc0ba9b15fafe04b9afb400872326d0ea289139
santiagopemo <santiagopemo@gmail.com>
2020-05-08 01:56:20 +0000

    added task 12

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def roman_to_int(roman_string):
     total = 0
-    if roman_string and len(roman_string) and isinstance(roman_string, str):
+    if roman_string and len(roman_string):
         numbers = {
             'I': 1,
             'V': 5,
@@ -14,9 +14,9 @@ def roman_to_int(roman_string):
         rom = roman_string
         l = len(rom)
         for i in range(l):
-            if rom[i] not in numbers:
+            if numbers.get(rom[i], 0) == 0:
                 return 0
-            elif i != l - 1 and (numbers[rom[i + 1]] > numbers[rom[i]]):
+            if i != l - 1 and (numbers[rom[i + 1]] > numbers[rom[i]]):
                 total -= numbers[rom[i]]
             else:
                 total += numbers[rom[i]]
