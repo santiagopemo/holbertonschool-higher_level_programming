588952b5a7b8086791184bd25f09336863a043fd
santiagopemo <santiagopemo@gmail.com>
2020-05-20 03:40:26 +0000

    added task 103

--- 0x06-python-classes/103-magic_class.py
+++ 0x06-python-classes/103-magic_class.py
@@ -4,7 +4,7 @@ import math
 
 
 class MagicClass:
-    def __init__(self, radius):
+    def __init__(self, radius=0):
         """ Initializates the instance """
         self.__radius = 0
         if type(radius) is not int and type(radius) is not float:
