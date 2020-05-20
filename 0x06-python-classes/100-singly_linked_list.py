37af82cbd027729c4a4dc674b54e7ecb2ce6afb7
santiagopemo <santiagopemo@gmail.com>
2020-05-20 00:44:57 +0000

    added task 100

--- 0x06-python-classes/100-singly_linked_list.py
+++ 0x06-python-classes/100-singly_linked_list.py
@@ -57,13 +57,13 @@ class SinglyLinkedList:
     def sorted_insert(self, value):
         """ Adds a new node sortedly in a linked list """
         new = Node(value)
-        if self.__head is None or new.data < self.__head.data:
+        if self.__head is None or new.data <= self.__head.data:
             new.next_node = self.__head
             self.__head = new
             return
         tmp = self.__head
         while tmp.next_node is not None:
-            if new.data < tmp.next_node.data:
+            if new.data <= tmp.next_node.data:
                 break
             tmp = tmp.next_node
         new.next_node = tmp.next_node
