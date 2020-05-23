d6c09180b33bdc87da9fed791cd353df12e83bee
santiagopemo <santiagopemo@gmail.com>
2020-05-23 07:58:48 +0000

    corrected if condition

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -1,13 +1,15 @@
 #!/usr/bin/python3
-"""Add two integers"""
+"""Add two numbers and check them"""
 
 
 def add_integer(a, b=98):
-    """ Adds two integers """
+    """Add a and b"""
+
     if type(a) is float:
         a = int(a)
     if type(b) is float:
         b = int(b)
+
     if not type(a) is int or type(a) is float:
         raise TypeError("a must be an integer")
     if not type(b) is int or type(b) is float:
