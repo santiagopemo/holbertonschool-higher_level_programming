3ccfa88cbd8989e7c0a5f1005d89f388cdf49f8b
santiagopemo <santiagopemo@gmail.com>
2020-06-03 23:53:23 +0000

    added task 12

---
+++ 0x0B-python-input_output/12-student.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""Student Module"""
+
+
+class Student:
+    """Student Class"""
+    def __init__(self, first_name, last_name, age):
+        """Initalizates a student"""
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """Retrieves a dictionary representation of a Student instance"""
+        if type(attrs) is list and all(type(i) is str for i in attrs):
+            new_dict = {}
+            for at in attrs:
+                if hasattr(self, at):
+                    new_dict[at] = self.__dict__[at]
+            return new_dict
+        return self.__dict__
