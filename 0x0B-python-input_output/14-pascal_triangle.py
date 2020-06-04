63e6f610ce95e98ba9f0c5b04310d80d060c696f
santiagopemo <santiagopemo@gmail.com>
2020-06-04 01:10:01 +0000

    added task 14

---
+++ 0x0B-python-input_output/14-pascal_triangle.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+"""Pascal Triangle Module"""
+
+
+def pascal_triangle(n):
+    """
+    returns a list of lists of integers representing
+    the Pascal’s triangle of n
+    """
+    if n <= 0:
+        return []
+    triangle = [[1]]
+    for i in range(1, n):
+        row = []
+        for j in range(i + 1):
+            prev_idx = j - 1
+            next_idx = j
+            if prev_idx == -1:
+                first = 0
+            else:
+                first = triangle[i - 1][prev_idx]
+            if next_idx == len(triangle[i - 1]):
+                second = 0
+            else:
+                second = triangle[i - 1][next_idx]            
+            row.append(first + second)
+        triangle.append(row)
+    return triangle
