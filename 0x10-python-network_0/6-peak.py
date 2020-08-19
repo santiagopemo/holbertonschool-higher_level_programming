3da27b06d2bbd6565f20f0307706b8c5eb1daedf
santiagopemo <santiagopemo@gmail.com>
2020-08-19 02:13:39 +0000

    added task 6

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -3,6 +3,8 @@
 
 
 def find_peak(list_of_integers):
-    """function that finds a peak in a list of unsorted integers"""    
-    if len(list_of_integers) != 0:
-        return max(list_of_integers)
+    """function that finds a peak in a list of unsorted integers"""
+    if len(list_of_integers) == 0:
+        return None
+    list_of_integers.sort()
+    return list_of_integers[-1]
