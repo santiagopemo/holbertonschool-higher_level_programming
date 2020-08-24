596dbec882d54849fbcc7d44dd0ef838268eb99c
santiagopemo <santiagopemo@gmail.com>
2020-08-24 21:05:01 +0000

    added task 6

--- 0x11-python-network_1/6-post_email.py
+++ 0x11-python-network_1/6-post_email.py
@@ -10,6 +10,6 @@ import sys
 
 if __name__ == "__main__":
     url = sys.argv[1]
-    payload = {'email': sys.argv[2]}
-    r = requests.post(url, params=payload)
+    data = {'email': sys.argv[2]}
+    r = requests.post(url, data)
     print(r.text)
