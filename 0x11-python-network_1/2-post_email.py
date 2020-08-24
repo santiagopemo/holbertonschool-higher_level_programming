c052c45339a55c0b283dccad84bc7a3f6653f7b5
santiagopemo <santiagopemo@gmail.com>
2020-08-24 18:22:28 +0000

    added task 2

--- 0x11-python-network_1/2-post_email.py
+++ 0x11-python-network_1/2-post_email.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 """
 script that takes in a URL and an email, sends a POST request to the passed
-URL with the email as a parameter, and displays the body of the response 
+URL with the email as a parameter, and displays the body of the response
 (decoded in utf-8)
 """
 import sys
@@ -11,5 +11,6 @@ import urllib.request
 if __name__ == "__main__":
     url = sys.argv[1]
     variables = {'email': sys.argv[2]}
+    req = urllib.request.Request(url, variables)
     with urllib.request.urlopen(url, variables) as response:
         print(response.read())
