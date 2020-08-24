5ddc7bb893ce013264095cf3fabfba14b2dea767
santiagopemo <santiagopemo@gmail.com>
2020-08-24 23:27:07 +0000

    added task 100

--- 0x11-python-network_1/100-github_commits.py
+++ 0x11-python-network_1/100-github_commits.py
@@ -10,8 +10,9 @@ if __name__ == "__main__":
     repo = sys.argv[1]
     owner = sys.argv[2]
     url = 'https://api.github.com/repos/{}/{}/commits'.format(owner, repo)
-    params = {'per_page': 10}
-    r = requests.get(url, params=params)
-    for c in r.json():
+    r = requests.get(url)
+    for i, c in enumerate(r.json()):
+        if i == 10:
+            break
         print("{}: {}".format(c.get("sha"),
                               c.get("commit").get("author").get("name")))
