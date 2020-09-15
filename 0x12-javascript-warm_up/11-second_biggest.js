3c12170327c523f62d088f2ff2a23504d53010c3
santiagopemo <santiagopemo@gmail.com>
2020-09-15 21:07:54 +0000

    added task 11

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -3,6 +3,6 @@ if (process.argv.length < 4) {
   console.log(0);
 } else {
   const args = process.argv
-    .slice(2).sort();
+    .slice(2).sort(function (a, b) { return a - b; });
   console.log(args[args.length - 2]);
 }
