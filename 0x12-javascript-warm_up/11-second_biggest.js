47b21b2306287c732b663277a9002720549c53ad
santiagopemo <santiagopemo@gmail.com>
2020-09-15 21:02:40 +0000

    added task 11

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -2,7 +2,7 @@
 if (process.argv.length < 4) {
   console.log(0);
 } else {
-  const args = process.argv.map(Number)
+  const args = process.argv
     .slice(2).sort(function (a, b) { return a - b; });
   console.log(args[args.length - 2]);
 }
